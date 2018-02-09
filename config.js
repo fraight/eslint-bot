2e8b963aa66698a21594668a02063526a8645577
Zeke Nierenberg <hampzan09@gmail.com>
2018-02-09 10:40:32 -0600

    adapt to read from env

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-export const GITHUB_USERNAME = 'username'; // Your bot's Github username
-export const GITHUB_PASSWORD = 'password'; // Your bot's Github password
+export const GITHUB_USERNAME = process.env.USERNAME;//'username'; // Your bot's Github username
+export const GITHUB_PASSWORD = process.env.PASSWORD;//'password'; // Your bot's Github password
 
-export const REPOSITORY_OWNER = 'owner/organisation';   // The owner of the repository you want to run the bot on.
-export const REPOSITORY_NAME = 'name';                  // The name of the repository you want to run the bot on.
+export const REPOSITORY_OWNER = process.env.ORG; //   // The owner of the repository you want to run the bot on.
+export const REPOSITORY_NAME = process.env.REPO; //   // The name of the repository you want to run the bot on.
 
 export const FILE_FILTER = /.*(.js|.jsx)$/; // By default, lint every single .js or .jsx file
